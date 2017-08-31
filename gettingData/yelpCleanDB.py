7d7fc73d5cac81cdd929e6859e63f5709730cd1f
Selin Erguncu <erguncu@usc.edu>
2017-08-30 23:51:20 -0700

    clean data DB updated

--- gettingData/yelpCleanDB.py
+++ gettingData/yelpCleanDB.py
@@ -1,4 +1,5 @@
 import sqlite3 as sqlite
+# import cleanData
 
 import os
 
@@ -11,6 +12,13 @@ cur = conn.cursor()
 cur.executescript('''
 
 DROP TABLE IF EXISTS CleanBusinessData;
+DROP TABLE IF EXISTS DistanceAllBusinesses;
+DROP TABLE IF EXISTS DistanceSelectedBusinesses;
+DROP TABLE IF EXISTS DataDistanceAdded;
+DROP TABLE IF EXISTS CompetitionZipcode;
+DROP TABLE IF EXISTS DataCompetitionZipcodeAdded;
+DROP TABLE IF EXISTS CompetitionCity;
+DROP TABLE IF EXISTS DataCompetitionCityAdded;
 
 
 CREATE TABLE CleanBusinessData (
@@ -30,9 +38,51 @@ CREATE TABLE CleanBusinessData (
   latitude INTEGER,
   query_price INTEGER,
   region TEXT
-)
+);
+
+CREATE TABLE DistanceAllBusinesses (
+  id TEXT,
+  rating INTEGER,
+  price TEXT,
+  distance INTEGER
+);
+
+CREATE TABLE DistanceSelectedBusinesses (
+  id TEXT,
+  closest2Distance INTEGER,
+  closest2Price INTEGER,
+  closest2Rating INTEGER,
+  closest5Distance INTEGER,
+  closest5Price INTEGER,
+  closest5Rating INTEGER,
+  closest10Distance INTEGER,
+  closest10Price INTEGER,
+  closest10Rating INTEGER,
+  closest15Distance INTEGER,
+  closest15Price INTEGER,
+  closest15Rating INTEGER
+);
+
+CREATE TABLE CompetitionZipcode (
+  zipcode TEXT,
+  numberOfCompetitorsZipcode INTEGER,
+  avgPriceZipcode INTEGER,
+  avgRatingZipcode INTEGER
+);
+
+
+
+CREATE TABLE CompetitionCity (
+  city TEXT,
+  numberOfCompetitorsCity INTEGER,
+  avgPriceCity INTEGER,
+  avgRatingCity INTEGER
+);
+
 
 ''')
 
 conn.commit()
 conn.close()
+
+# cleanData.writeFinalDataToDB()
