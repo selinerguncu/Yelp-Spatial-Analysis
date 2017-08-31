9023261de2485b9a843ac6ea6fd947ca5824c117
Selin Erguncu <erguncu@usc.edu>
2017-08-30 23:52:57 -0700

    updated data preperation - writing to the DB now

--- analysis/dataPrep.py
+++ analysis/dataPrep.py
@@ -31,36 +31,58 @@ def distanceFromTheClosest(data):
   zipcodes = data['zipcode'].unique()
   categories = data['query_category'].unique()
   # print(zipcodes)
-  distanceAllBusinesses = pd.DataFrame(columns = ['distance', 'id', 'price', 'rating'])
+  conn = sqlite.connect("/Users/selinerguncu/Desktop/PythonProjects/Fun Projects/Yelp/data/yelpCleanDB.sqlite")
+  cur = conn.cursor()
+  # distanceAllBusinesses = pd.read_sql_query("SELECT * FROM DistanceAllBusinesses", conn)
+  # distanceAllBusinesses = pd.DataFrame(columns = ['distance', 'id', 'price', 'rating'])
   for category in categories:
     print(category)
-    for zipcode in zipcodes[0:5]:
+    for zipcode in zipcodes:
       print(zipcode)
       regionalData = data[(data['zipcode'] == zipcode) & (data['query_category'] == category)]
-      for row in regionalData.itertuples():
-        coordinate1 = (row[14], row[12])
+      # for row in regionalData.itertuples():
+      for index, row in regionalData.iterrows():
+        coordinate1 = (row['latitude'], row['longitude'])
+        # coordinate1 = (row[14], row[12])
         # print('row', row)
         # sys.exit()
       # for row_id, row in enumerate(data.values):
-        for i in regionalData.itertuples():
-          if row != i:
-            coordinate2 = (i[14], i[12])
+        # for i in regionalData.itertuples():
+        for index, i in regionalData.iterrows():
+          if row['id'] != i['id']:
+          # if row != i:
+            coordinate2 = (i['latitude'], i['longitude'])
+            # coordinate2 = (i[14], i[12])
+            # distance = great_circle(coordinate1, coordinate2).miles
+            # businessID = row[1]
+            # price = int(i[15])
+            # rating = i[2]
             distance = great_circle(coordinate1, coordinate2).miles
-            businessID = row[1]
-            price = int(i[15])
-            rating = i[2]
+            businessID = row['id']
+            price = int(i['query_price'])
+            rating = i['rating']
             # temp.extend([distance, businessID, price, rating])
             # distanceAllBusinesses.append(temp)
-            distanceAllBusinesses.loc[len(distanceAllBusinesses)] = [distance, businessID, price, rating]
+            cur.execute('''INSERT INTO DistanceAllBusinesses(distance, id, price, rating)
+              VALUES (?, ?, ?, ?)''', (distance, businessID, price, rating))
+            # cur.execute('''UPDATE DistanceAllBusinesses SET distance = ?
+            #   WHERE id = ? AND price = ? AND rating = ?''', (distance, businessID, price, rating))
+  conn.commit()
+            # distanceAllBusinesses.loc[len(distanceAllBusinesses)] = [distance, businessID, price, rating]
             # print('i', distanceAllBusinesses)
-            # sys.exit()
-
   # sys.exit()
 
+  # columns = ['id', 'closest2Distance','closest2Price','closest2Rating',
+  #                    'closest5Distance','closest5Price','closest5Rating',
+  #                    'closest10Distance','closest10Price','closest10Rating',
+  #                    'closest15Distance','closest15Price','closest15Rating']
+  # distanceSelectedBusinesses = pd.DataFrame(columns = columns)
   businesses = data['id'].unique()
-  for business in businesses[0:10]:
+  distanceAllBusinesses = pd.read_sql_query("SELECT * FROM DistanceAllBusinesses", conn)
+  for business in businesses[0:100]:
+    businessID = business
     closest2 = distanceAllBusinesses[distanceAllBusinesses['id'] == business].nsmallest(2, 'distance')
-    print(closest2.head())
+    # print(closest2.head())
     closest2Distance = closest2['distance'].mean()
     closest2Price = closest2['price'].mean()
     closest2Rating = closest2['rating'].mean()
@@ -80,107 +102,136 @@ def distanceFromTheClosest(data):
     closest15Price = closest15['price'].mean()
     closest15Rating = closest15['rating'].mean()
 
-    columns = ['closest2Distance','closest2Price','closest2Rating',
-               'closest5Distance','closest5Price','closest5Rating',
-               'closest10Distance','closest10Price','closest10Rating',
-               'closest15Distance','closest15Price','closest15Rating']
+    cur.execute('''INSERT INTO DistanceSelectedBusinesses(closest2Distance, closest2Price, closest2Rating,
+      closest5Distance, closest5Price, closest5Rating,
+      closest10Distance, closest10Price, closest10Rating,
+      closest15Distance, closest15Price, closest15Rating, id)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+      (closest2Distance, closest2Price, closest2Rating,
+        closest5Distance, closest5Price, closest5Rating,
+        closest10Distance, closest10Price, closest10Rating,
+        closest15Distance, closest15Price, closest15Rating, businessID))
+  conn.commit()
 
-    # dataDistanceAdded = pd.concat([distanceAllBusinesses, pd.DataFrame(columns = columns)])
+    # distanceSelectedBusinesses.loc[len(distanceSelectedBusinesses)] = [businessID, closest2Distance, closest2Price, closest2Rating, closest5Distance, closest5Price, closest5Rating, closest10Distance, closest10Price, closest10Rating, closest15Distance, closest15Price, closest15Rating]
 
-    temp = pd.DataFrame([[0,0,0,0,0,0,0,0,0,0,0,0]],
-      index=distanceAllBusinesses.index, columns = columns)
+  # merge data with distanceSelectedBusiness:
+  distanceSelectedBusinesses = pd.read_sql_query("SELECT * FROM DistanceSelectedBusinesses", conn)
 
-    dataDistanceAdded = data.join(temp, how='outer')
-    print(dataDistanceAdded.head())
+  dataDistanceAdded = pd.merge(data, distanceSelectedBusinesses, how='inner', on='id',
+    left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=False)
 
-    for index, row in dataDistanceAdded.iterrows():
-      if row['id'] == business:
-        row['closest2Distance'] = closest2Distance
-        row['closest2Price'] = closest2Price
-        row['closest2Rating'] = closest2Rating
-        row['closest5Distance'] = closest5Distance
-        row['closest5Price'] = closest5Price
-        row['closest5Rating'] = closest5Rating
-        row['closest10Distance'] = closest10Distance
-        row['closest10Price'] = closest10Price
-        row['closest10Rating'] = closest10Rating
-        row['closest15Distance'] = closest15Distance
-        row['closest15Price'] = closest15Price
-        row['closest15Rating'] = closest15Rating
-
-        # row['query_price'] = int(row['query_price'])
-        # print(row)
-
+  dataDistanceAdded.to_sql('DataDistanceAdded', conn)
+  conn.commit()
+  print('step 2 finished')
   return dataDistanceAdded
 
 
 def addCompetitionZipcode(data):
-  # data['numberOfCompetitorsZipcode'] = 0
-  # data['avgPriceZipcode'] = 0
-  # data['avgRatingZipcode'] = 0
-
-  columns = ['numberOfCompetitorsZipcode','avgPriceZipcode','avgRatingZipcode']
-
-  # dataCompetitionZipcodeAdded = pd.concat([data, pd.DataFrame(columns = columns)])
-
-  temp = pd.DataFrame([[0,0,0]], index=data.index, columns = columns)
-
-  dataCompetitionZipcodeAdded = data.join(temp, how='outer')
-
+  # columns = ['zipcode', 'numberOfCompetitorsZipcode','avgPriceZipcode','avgRatingZipcode']
+  # dataCompetitionZipcode = pd.DataFrame(columns = columns)
+  conn = sqlite.connect("/Users/selinerguncu/Desktop/PythonProjects/Fun Projects/Yelp/data/yelpCleanDB.sqlite")
+  cur = conn.cursor()
   zipcodes = data['zipcode'].unique()
   categories = data['query_category'].unique()
 
   for category in categories:
-    for zipcode in zipcodes[0:1]:
-      regionalData = dataCompetitionZipcodeAdded[(dataCompetitionZipcodeAdded['zipcode'] == zipcode) & (dataCompetitionZipcodeAdded['query_category'] == category)]
+    for zipcode in zipcodes:
+      regionalData = data[(data['zipcode'] == zipcode) & (data['query_category'] == category)]
+
+      zipcode = zipcode
       numberOfCompetitorsZipcode = len(regionalData.index)
       avgPriceZipcode = regionalData['query_price'].mean()
       avgRatingZipcode = regionalData['rating'].mean()
 
-      for index, row in dataCompetitionZipcodeAdded.iterrows():
-        if row['zipcode'] == zipcode and row['query_category'] == category:
-          row['numberOfCompetitorsZipcode'] = numberOfCompetitorsZipcode
-          row['avgPriceZipcode'] = avgPriceZipcode
-          row['avgRatingZipcode'] = avgRatingZipcode
-        if row['numberOfCompetitorsZipcode'] != 0 and row['closest10Price'] != 0 and row['closest10Price'] != np.nan:
-          print(row)
-  # data = dataCompetitionZipcodeAdded
-  print(dataCompetitionZipcodeAdded.head())
+      cur.execute('''INSERT INTO CompetitionZipcode(numberOfCompetitorsZipcode,
+        avgPriceZipcode, avgRatingZipcode, zipcode) VALUES (?, ?, ?, ?)''',
+        (numberOfCompetitorsZipcode, avgPriceZipcode, avgRatingZipcode, zipcode))
+  conn.commit()
+      # dataCompetitionZipcode.loc[len(dataCompetitionZipcode)] = [zipcode, numberOfCompetitorsZipcode, avgPriceZipcode, avgRatingZipcode]
+
+  competitionZipcode = pd.read_sql_query("SELECT * FROM CompetitionZipcode", conn)
+  dataCompetitionZipcodeAdded = pd.merge(data, competitionZipcode, how='inner', on='zipcode',
+    left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=False)
+
+
+  dataCompetitionZipcodeAdded.to_sql('DataCompetitionZipcodeAdded', conn)
+  conn.commit()
+  # print(dataCompetitionZipcodeAdded.head())
+  print('step 3 finished')
+
   return dataCompetitionZipcodeAdded
 # addCompetitionZipcode(distanceFromTheClosest(getCleanData()))
 # sys.exit()
 
+
 def addCompetitionCity(data):
-  columns = ['numberOfCompetitorsCity','avgPriceCity','avgRatingCity']
-
-  temp = pd.DataFrame([[0,0,0]], index=data.index, columns = columns)
-
-  dataCompetitionCityAdded = data.join(temp, how='outer')
-
+  # columns = ['city', 'numberOfCompetitorsCity','avgPriceCity','avgRatingCity']
+  # dataCompetitionCity = pd.DataFrame(columns = columns)
+  conn = sqlite.connect("/Users/selinerguncu/Desktop/PythonProjects/Fun Projects/Yelp/data/yelpCleanDB.sqlite")
+  cur = conn.cursor()
   cities = data['city'].unique()
   categories = data['query_category'].unique()
 
   for category in categories:
-    for city in cities[0:1]:
-      regionalData = dataCompetitionCityAdded[(dataCompetitionCityAdded['city'] == city) & (dataCompetitionCityAdded['query_category'] == category)]
+    for city in cities:
+      regionalData = data[(data['city'] == city) & (data['query_category'] == category)]
+
+      city = city
       numberOfCompetitorsCity = len(regionalData.index)
       avgPriceCity = regionalData['query_price'].mean()
       avgRatingCity = regionalData['rating'].mean()
 
-      for index, row in dataCompetitionCityAdded.iterrows():
-        if row['city'] == city and row['query_category'] == category:
-          row['numberOfCompetitorsCity'] = numberOfCompetitorsCity
-          row['avgPriceCity'] = avgPriceCity
-          row['avgRatingCity'] = avgRatingCity
-        if row['numberOfCompetitorsCity'] != 0 and row['numberOfCompetitorsCity'] != 0 and row['closest10Price'] != np.nan:
-          print(row)
+      cur.execute('''INSERT INTO CompetitionCity(numberOfCompetitorsCity,
+        avgPriceCity, avgRatingCity, city) VALUES (?, ?, ?, ?)''',
+        (numberOfCompetitorsCity, avgPriceCity, avgRatingCity, city))
+  conn.commit()
+      # dataCompetitionCity.loc[len(dataCompetitionCity)] = [city, numberOfCompetitorsCity, avgPriceCity, avgRatingCity]
 
-  # data = dataCompetitionCityAdded
-  print(dataCompetitionCityAdded.columns)
+  competitionCity = pd.read_sql_query("SELECT * FROM CompetitionCity", conn)
+  dataCompetitionCityAdded = pd.merge(data, competitionCity, how='inner', on='city',
+    left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=False)
+
+  dataCompetitionCityAdded.to_sql('DataCompetitionCityAdded', conn)
+  print('step 4 finished')
+  conn.commit()
   return dataCompetitionCityAdded
 
-addCompetitionCity(addCompetitionZipcode(distanceFromTheClosest(getCleanData())))
 
+dataCompetitionCityAdded = addCompetitionCity(addCompetitionZipcode(distanceFromTheClosest(getCleanData())))
+
+print(dataCompetitionCityAdded.head())
+
+#### OLD CODE ##### doesn't work for merging - 0 or None values come for the newly added cells
+
+# def addCompetitionCity(data):
+#   columns = ['numberOfCompetitorsCity','avgPriceCity','avgRatingCity']
+
+#   temp = pd.DataFrame([[0,0,0]], index=data.index, columns = columns)
+
+#   dataCompetitionCityAdded = data.join(temp, how='outer')
+
+#   cities = data['city'].unique()
+#   categories = data['query_category'].unique()
+
+#   for category in categories:
+#     for city in cities[0:1]:
+#       regionalData = dataCompetitionCityAdded[(dataCompetitionCityAdded['city'] == city) & (dataCompetitionCityAdded['query_category'] == category)]
+#       numberOfCompetitorsCity = len(regionalData.index)
+#       avgPriceCity = regionalData['query_price'].mean()
+#       avgRatingCity = regionalData['rating'].mean()
+
+#       for index, row in dataCompetitionCityAdded.iterrows():
+#         if row['city'] == city and row['query_category'] == category:
+#           row['numberOfCompetitorsCity'] = numberOfCompetitorsCity
+#           row['avgPriceCity'] = avgPriceCity
+#           row['avgRatingCity'] = avgRatingCity
+#         if row['numberOfCompetitorsCity'] != 0 and row['numberOfCompetitorsCity'] != 0 and row['closest10Price'] != np.nan:
+#           print(row)
+
+#   # data = dataCompetitionCityAdded
+#   print(dataCompetitionCityAdded.columns)
+#   return dataCompetitionCityAdded
 
 # id  rating price        county
 # query_latitude  query_longitude  population          city  review_count
@@ -197,3 +248,110 @@ addCompetitionCity(addCompetitionZipcode(distanceFromTheClosest(getCleanData()))
 #        'numberOfCompetitorsZipcode', 'avgPriceZipcode', 'avgRatingZipcode',
 #        'numberOfCompetitorsCity', 'avgPriceCity', 'avgRatingCity'],
 #       dtype='object')
+
+
+# CREATE TABLE DataDistanceAdded (
+#   id TEXT,
+#   rating INTEGER,
+#   price TEXT,
+#   county TEXT,
+#   query_latitude INTEGER,
+#   query_longitude INTEGER,
+#   population INTEGER,
+#   city TEXT,
+#   review_count INTEGER,
+#   area INTEGER,
+#   zipcode TEXT,
+#   longitude INTEGER,
+#   query_category TEXT,
+#   latitude INTEGER,
+#   query_price INTEGER,
+#   region TEXT,
+#   distance INTEGER,
+#   closest2Distance INTEGER,
+#   closest2Price INTEGER,
+#   closest2Rating INTEGER,
+#   closest5Distance INTEGER,
+#   closest5Price INTEGER,
+#   closest5Rating INTEGER,
+#   closest10Distance INTEGER,
+#   closest10Price INTEGER,
+#   closest10Rating INTEGER,
+#   closest15Distance INTEGER,
+#   closest15Price INTEGER,
+#   closest15Rating INTEGER
+# );
+
+# CREATE TABLE DataCompetitionZipcodeAdded (
+#   id TEXT,
+#   rating INTEGER,
+#   price TEXT,
+#   county TEXT,
+#   query_latitude INTEGER,
+#   query_longitude INTEGER,
+#   population INTEGER,
+#   city TEXT,
+#   review_count INTEGER,
+#   area INTEGER,
+#   zipcode TEXT,
+#   longitude INTEGER,
+#   query_category TEXT,
+#   latitude INTEGER,
+#   query_price INTEGER,
+#   region TEXT,
+#   distance INTEGER
+#   closest2Distance INTEGER,
+#   closest2Price INTEGER,
+#   closest2Rating INTEGER,
+#   closest5Distance INTEGER,
+#   closest5Price INTEGER,
+#   closest5Rating INTEGER,
+#   closest10Distance INTEGER,
+#   closest10Price INTEGER,
+#   closest10Rating INTEGER,
+#   closest15Distance INTEGER,
+#   closest15Price INTEGER,
+#   closest15Rating INTEGER,
+#   numberOfCompetitorsZipcode INTEGER,
+#   avgPriceZipcode INTEGER,
+#   avgRatingZipcode INTEGER
+# );
+
+
+# CREATE TABLE DataCompetitionCityAdded (
+#   id TEXT,
+#   rating INTEGER,
+#   price TEXT,
+#   county TEXT,
+#   query_latitude INTEGER,
+#   query_longitude INTEGER,
+#   population INTEGER,
+#   city TEXT,
+#   review_count INTEGER,
+#   area INTEGER,
+#   zipcode TEXT,
+#   longitude INTEGER,
+#   query_category TEXT,
+#   latitude INTEGER,
+#   query_price INTEGER,
+#   region TEXT,
+#   distance INTEGER
+#   closest2Distance INTEGER,
+#   closest2Price INTEGER,
+#   closest2Rating INTEGER,
+#   closest5Distance INTEGER,
+#   closest5Price INTEGER,
+#   closest5Rating INTEGER,
+#   closest10Distance INTEGER,
+#   closest10Price INTEGER,
+#   closest10Rating INTEGER,
+#   closest15Distance INTEGER,
+#   closest15Price INTEGER,
+#   closest15Rating INTEGER,
+#   numberOfCompetitorsZipcode INTEGER,
+#   avgPriceZipcode INTEGER,
+#   avgRatingZipcode INTEGER,
+#   numberOfCompetitorsCity INTEGER,
+#   avgPriceCity INTEGER,
+#   avgRatingCity INTEGER
+# );
